6748f73f24c5764b3176a437e4c713374fd98a6a
jefmud <jefmud@gmail.com>
2020-02-11 12:30:56 -0500

    Add files via upload

---
+++ chemsort/simple_authentication - Copy.py
@@ -0,0 +1,18 @@
+# a simple-minded authentication module, replace in production
+# probably OAuth2 would be nice here.
+
+# change to valid values, don't use any valid account passwords!
+# since the form has a email validator, you would have to use an email
+# as the username
+userhive = [
+    {'username': 'your-email', 'password': 'your-password'},
+    {'username': 'your-email2', 'password': 'your-password2'}
+]
+
+def authenticate(username, password):
+    for user in userhive:
+        if user['username'] == username:
+            if user['password'] == password:
+                return True
+    return False
+        
